a2961b50666ae7419e6594e9ef74f22377383656
Sam <samjnewby@hotmail.com>
2026-06-15 20:26:46 -0400

    feat: per-NPC kills on the stored trip detail

--- src/main/java/com/goodrunetracker/adapter/SessionHistory.java
+++ src/main/java/com/goodrunetracker/adapter/SessionHistory.java
@@ -72,7 +72,7 @@ public final class SessionHistory {
                 ItemValuer v = new FrozenItemValuer(SessionMapper.unitPrices(st));
                 return new TripDetail(lines(t.pickedUp(), v), lines(t.missed(), v),
                         lines(t.suppliesUsed(), v), t.netProfit(v), t.missedValue(v),
-                        SkillXp.sortedFrom(t.xpGained()));
+                        SkillXp.sortedFrom(t.xpGained()), NpcKills.sortedByCountDesc(t.kills()));
             }
         }
         return null;
@@ -252,16 +252,18 @@ public final class SessionHistory {
         public final long netProfit;
         public final long missedValue;
         public final List<SkillXp> xpGained;
+        public final List<NpcKills> killsByNpc;
 
         public TripDetail(List<ItemLine> pickedUp, List<ItemLine> leftOnGround,
                           List<ItemLine> suppliesUsed, long netProfit, long missedValue,
-                          List<SkillXp> xpGained) {
+                          List<SkillXp> xpGained, List<NpcKills> killsByNpc) {
             this.pickedUp = pickedUp;
             this.leftOnGround = leftOnGround;
             this.suppliesUsed = suppliesUsed;
             this.netProfit = netProfit;
             this.missedValue = missedValue;
             this.xpGained = xpGained;
+            this.killsByNpc = killsByNpc;
         }
     }
 
